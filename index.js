2f2502e04b2d6ddf4d04b1914cbfd25e20c6ddff
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-04-29 12:32:12 +0600

    update method is complate

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.POST || 5000;
 //middleware
 app.use(cors())
 app.use(express.json())
-{console.log(process.env.DB_USER);}
-{console.log(process.env.DB_PASS);}
+{ console.log(process.env.DB_USER); }
+{ console.log(process.env.DB_PASS); }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gphdl2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,37 +29,61 @@ async function run() {
     const paperCollection = client.db("paperCollection").collection("paperData");
 
 
-    app.post('/craftItems', async(req, res) => {
-        const data = req.body;
-        const result = await paperCollection.insertOne(data);
-        res.send(result)
+    app.post('/craftItems', async (req, res) => {
+      const data = req.body;
+      const result = await paperCollection.insertOne(data);
+      res.send(result)
     })
 
-    app.get('/craftItems', async(req, res) => {
-        const cursor = paperCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/craftItems', async (req, res) => {
+      const cursor = paperCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/craftItems/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await paperCollection.findOne(query);
-        res.send(result);
+    app.get('/craftItems/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await paperCollection.findOne(query);
+      res.send(result);
     })
 
-    app.get('/craftItemsInfo/:email', async(req, res) => {
-        const myEmaill = req.params.email;
-        const filter = { email: myEmaill }
-        const result = await paperCollection.find(filter).toArray()
-        res.send(result);
+    app.put('/craftItems/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateInfo = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          photo: updateInfo.photo,
+          itemName: updateInfo.itemName,
+          subCategory: updateInfo.subCategory,
+          shortDescription: updateInfo.shortDescription,
+          price: updateInfo.price,
+          rating: updateInfo.rating,
+          processingTime: updateInfo.processingTime,
+          stockStatus: updateInfo.stockStatus,
+          customization: updateInfo.customization,
+          email: updateInfo.email,
+          displayName: updateInfo.displayName
+        }
+      };
+      const result = await paperCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
     })
 
-    app.delete('/craftItemsInfo/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await paperCollection.deleteOne(query);
-        res.send(result)
+    app.get('/craftItemsInfo/:email', async (req, res) => {
+      const myEmaill = req.params.email;
+      const filter = { email: myEmaill }
+      const result = await paperCollection.find(filter).toArray()
+      res.send(result);
+    })
+
+    app.delete('/craftItemsInfo/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await paperCollection.deleteOne(query);
+      res.send(result)
     })
 
 
